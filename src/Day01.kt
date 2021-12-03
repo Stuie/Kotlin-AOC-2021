0ba2e13029b15cb15cd2d518e91f40429d5843b5
Stuart Gilbert <stuart.gilbert@gmail.com>
2021-12-02 21:55:06 -0800

    Day 1 Complete
    forEachIndexed solution for both parts.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increaseCount = 0
+        var previous = 0
+        input.forEachIndexed { index, item ->
+            if (index == 0) return@forEachIndexed
+            val current = Integer.parseInt(item)
+
+            if (current > previous) {
+                increaseCount++
+            }
+
+            previous = current
+        }
+
+        return increaseCount
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var increaseCount = 0
+        var previousSum = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        input.forEachIndexed { index, item ->
+            if (index == 0) return@forEachIndexed
+            if (input.size < index + 3) return@forEachIndexed
+
+            val first = Integer.parseInt(item)
+            val second = Integer.parseInt(input[index+1])
+            val third = Integer.parseInt(input[index+2])
+
+            val currentSum = first + second + third
+
+            if (currentSum > previousSum) increaseCount++
+
+            previousSum = currentSum
+        }
+
+        return increaseCount
+    }
 
     val input = readInput("Day01")
     println(part1(input))
